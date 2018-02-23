41185e05a0a208ee4b5d0aa61512606562904d72
davyvanroy <davy.van.roy@gmail.com>
2018-02-23 14:12:49 +0100

    initial commit

---
+++ src/main/java/io/fundrequest/bounty/BountyApplication.java
@@ -0,0 +1,12 @@
+package io.fundrequest.bounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BountyApplication.class, args);
+	}
+}
