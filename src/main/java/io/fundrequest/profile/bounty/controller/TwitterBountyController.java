553d4571b2abeafe004d5f5ad078a0e436c877e2
Quinten De Swaef <quintendeswaef@gmail.com>
2018-02-28 14:49:19 +0100

    tempfix

--- src/main/java/io/fundrequest/profile/bounty/controller/TwitterBountyController.java
+++ src/main/java/io/fundrequest/profile/bounty/controller/TwitterBountyController.java
@@ -32,6 +32,6 @@ public class TwitterBountyController {
 //
 //        }
 //        twitterService.userIsFollowing(profileService.getUserProfile(request, principal).getTwitter())
-        return "OK";
+        return true;
     }
 }
