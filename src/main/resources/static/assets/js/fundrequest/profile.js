8c636df9772a7da4b44e95866e9fe5e71edef108
Quinten De Swaef <quintendeswaef@gmail.com>
2018-03-01 16:13:52 +0100

    done twitter

--- src/main/resources/static/assets/js/fundrequest/profile.js
+++ src/main/resources/static/assets/js/fundrequest/profile.js
@@ -1,7 +1,20 @@
 (function () {
-  $(document).ready(function () {
+	$(document).ready(function () {
 
+		$('#tweetNow').click(function () {
+			console.log('lol');
+			var textToTweet = $('#tweetContent').val();
+			var twtLink = 'http://twitter.com/home?status=' + encodeURIComponent(textToTweet);
+			var twitter = window.open(twtLink, '_blank');
+			var winTimer = window.setInterval(function()
+			{
+				if (twitter.closed !== false)
+				{
+					window.clearInterval(winTimer);
+					$('#twitterClaimForm').submit();
+				}
+			}, 200);
+		});
 
-
-  });
+	});
 })();
