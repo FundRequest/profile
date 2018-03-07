724f1a25ac05c71254d09eaf47556c8a32cff9ba
davyvanroy <davy.van.roy@gmail.com>
2018-03-07 10:50:26 +0100

    referral count immediately visible

--- src/main/resources/static/assets/js/fundrequest/referral.js
+++ src/main/resources/static/assets/js/fundrequest/referral.js
@@ -3,9 +3,5 @@ define(["jquery"], function($) {
         $.get("/referrals", function(data) {
             $("#rewards-list-content").html(data);
         });
-
-        $.get("/referrals/total", function(data) {
-            $("#referral-total").html(data);
-        });
     });
 });
