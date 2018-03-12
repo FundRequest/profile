4dab61fd010e16850834525947a3824d32db10de
Steve Maris <steve_maris@hotmail.com>
2018-03-12 18:28:44 +0100

    Use gulp

---
+++ src/main/resources/design/gulpfile.js
@@ -0,0 +1,113 @@
+// fetch command line arguments
+const arg = (argList => {
+    let arg = {}, a, opt, thisOpt, curOpt;
+    for (a = 0; a < argList.length; a++) {
+
+        thisOpt = argList[a].trim();
+        opt = thisOpt.replace(/^\-+/, '');
+
+        if (opt === thisOpt) {
+
+            // argument value
+            if (curOpt) arg[curOpt] = opt;
+            curOpt = null;
+
+        }
+        else {
+
+            // argument name
+            curOpt = opt;
+            arg[curOpt] = true;
+        }
+    }
+    return arg;
+})(process.argv);
+
+
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    cssmin = require('gulp-cssnano'),
+    prefix = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    sassLint = require('gulp-sass-lint'),
+    sourcemaps = require('gulp-sourcemaps');
+tildeImporter = require('node-sass-tilde-importer');
+
+var target = "../static.assets/css";
+
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+runSequence = require('run-sequence');
+
+var displayError = function(error) {
+    // Initial building up of the error
+    var errorString = '[' + error.plugin.error.bold + ']';
+    errorString += ' ' + error.message.replace("\n", ''); // Removes new line at the end
+
+    // If the error contains the filename or line number add it to the string
+    if (error.fileName)
+        errorString += ' in ' + error.fileName;
+
+    if (error.lineNumber)
+        errorString += ' on line ' + error.lineNumber.bold;
+
+    // This will output an error like the following:
+    // [gulp-sass] error message in file_name on line 1
+    console.error(errorString);
+};
+
+var onError = function(err) {
+    notify.onError({
+        title: "Gulp",
+        subtitle: "Failure!",
+        message: "Error: <%= error.message %>",
+        sound: "Basso"
+    })(err);
+    this.emit('end');
+};
+
+var sassOptions = {
+    outputStyle: 'expanded',
+    importer: tildeImporter
+};
+
+var prefixerOptions = {
+    browsers: ['last 2 versions']
+};
+
+gulp.task('styles-general', function() {
+    return gulp.src('scss/general.scss')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions))
+        .pipe(prefix(prefixerOptions))
+        .pipe(gulp.dest(target))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(target))
+});
+
+gulp.task('styles-website', function() {
+    return gulp.src('scss/website.scss')
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions))
+        .pipe(prefix(prefixerOptions))
+        .pipe(gulp.dest(target))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(target))
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch(['scss/**/*.scss', '!scss/fundrequest/website/*.scss'], ['styles-general']);
+    gulp.watch('scss/fundrequest/website/*.scss', ['styles-website']);
+});
+
+gulp.task('default', function(done) {
+    target = (arg && arg.target) || target;
+    runSequence('styles-general', 'styles-website', 'watch', done);
+});
